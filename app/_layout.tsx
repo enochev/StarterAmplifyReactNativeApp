b73ed6efaf5184c9c055c66d9e795bbc83bbe900
evanenochs <evan.enochs@gmail.com>
2024-06-10 19:37:19 -0400

    Added Amplify project, sign in screen, set up lint

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,16 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { Slot } from "expo-router";
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react-native";
+import outputs from "../amplify_outputs.json";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+Amplify.configure(outputs);
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <Authenticator.Provider>
+      <Authenticator>
+        <Slot/>
+      </Authenticator>
+    </Authenticator.Provider>
   );
 }
